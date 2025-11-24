5b0360be5df925448ca86c51a41ee544b6613998
AlexLopez75 <alex.lopez.7e9@itb.cat>
2025-11-24 12:45:23 +0100

    fix: updated code section to latest installment.

--- Program.cs
+++ Program.cs
@@ -21,21 +21,22 @@ public class Program
 
         //Chapter 1:
         const string MsgInputName = "Enter your wizard's name: ";
-        const string MsgNameConfirm = "Your wizard's name is: ";
+        const string MsgNameConfirm = "Your wizard's name is: {0}";
         const string InputNameError = "Input a name without spaces.";
         const string PressEnter = "Press enter to continue";
+        const string Title = "Title: ";
         const string LevelDisaster = "You repeat the 2nd call";
         const string LevelNoob = "You still confuse your wand with a spoon";
         const string LevelNovice = "You are a Magic Breeze Caster";
         const string LevelAdvanced = "Wow! You can cast dragons witout burning the lab!";
         const string LevelMaster = "You have achieved the Master of Arcanes rank!";
         const string TitleDisaster = "Raoden the Elantrin";
-        const string TitleNoob = "Level: Zyn the bugged";
-        const string TitleNovice = "Level: Arka Nullpointer";
-        const string TitleAdvanced = "Level: Elarion of the Embers";
-        const string TitleMaster = "Level: ITB-Wizard the Grey";
+        const string TitleNoob = "Zyn the bugged";
+        const string TitleNovice = "Arka Nullpointer";
+        const string TitleAdvanced = "Elarion of the Embers";
+        const string TitleMaster = "ITB-Wizard the Grey";
         const string DayTraining = "Day {0} : {1} meditated for {2} hours and gained {3} power points.";
-        const string TotalTraining = "{0} has meditated for a total of {1} hours and gained {2} power points";
+        const string TotalTraining = "{0} has meditated for a total of {1} hours and gained {2} power points.";
 
         //Chapter 2
         const string MonsterEncounter = "A wild {0} appears! Rolling dice to determine the outcome of the battle...";
@@ -48,7 +49,7 @@ public class Program
         const string DiceRoll6 = "   ________\r\n  /       /|   \r\n /_______/ |\r\n | o   o | |\r\n | o   o | /\r\n | o   o |/ \r\n '-------'\r\n";
         const string DiceResult = "You rolled a {0}!";
         const string MonsterDamage = "The monster takes {0} damage.";
-        const string MonsterDefeat = "You have defeated the monster, you level increasses to level {0}.";
+        const string MonsterDefeat = "You have defeated the monster, your level increasses to level {0}.";
         const string MaxLevel = "You have achieved max level.";
         const int MinMonster = 0;
         const int MaxMonster = 8;
@@ -61,8 +62,8 @@ public class Program
 
         //Chapter 1:
         int day, hour, power, totalHour = 0, totalPower = 0, level = 1;
-        string wizardName = "none",  title = "none", space = " ";
-        bool validInput, badName = false;
+        string wizardName = "none", title = "none", space = " ";
+        bool validInput = true, badName = false;
 
         //Chapter 2:
         int[] arrayHp = { 3, 5, 10, 11, 18, 15, 20, 50 };
@@ -74,10 +75,12 @@ public class Program
 
         do
         {
+            Console.ForegroundColor = ConsoleColor.DarkYellow;
+            validInput = true;
             Console.WriteLine(MenuTitle);
             if (setWizard)
             {
-                Console.WriteLine(PlayerTitle, wizardName, title);
+                Console.WriteLine(PlayerTitle, wizardName, title, level);
             }
             Console.WriteLine(MenuOption1);
             Console.WriteLine(MenuOption2);
@@ -88,8 +91,8 @@ public class Program
             Console.WriteLine(MenuOption7);
             Console.WriteLine(MenuOptionExit);
             Console.Write(MenuPrompt);
-
-            validInput = true;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.WriteLine();
 
             try
             {
@@ -108,15 +111,18 @@ public class Program
 
             if (validInput)
             {
-                Console.WriteLine(op);
                 switch (op)
                 {
                     case 1:
                         do
                         {
+                            badName = false;
                             Console.Write(MsgInputName);
+                            Console.ForegroundColor = ConsoleColor.Blue;
                             wizardName = Console.ReadLine();
                             wizardName = char.ToUpper(wizardName[0]) + wizardName.Substring(1);
+                            Console.WriteLine();
+                            Console.ForegroundColor = ConsoleColor.White;
                             if (badName = wizardName.Contains(space))
                             {
                                 Console.WriteLine(InputNameError);
@@ -124,7 +130,8 @@ public class Program
                             }
                         } while (badName);
 
-                        Console.WriteLine(MsgNameConfirm + wizardName);
+                        Console.WriteLine(MsgNameConfirm, wizardName);
+                        Console.WriteLine();
 
                         for (day = 1; day < 6; day++)
                         {
@@ -141,30 +148,41 @@ public class Program
                         {
                             case >= 40:
                                 Console.WriteLine(LevelMaster);
-                                Console.WriteLine(TitleMaster);
-                                title = "ITB-Wizard the Grey";
+                                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                                Console.WriteLine(Title + TitleMaster);
+                                Console.ForegroundColor = ConsoleColor.White;
+                                title = TitleMaster;
                                 break;
                             case >= 35:
                                 Console.WriteLine(LevelAdvanced);
-                                Console.WriteLine(TitleAdvanced);
-                                title = "Elarion of the Embers";
+                                Console.ForegroundColor = ConsoleColor.Green;
+                                Console.WriteLine(Title + TitleAdvanced);
+                                Console.ForegroundColor = ConsoleColor.White;
+                                title = TitleAdvanced;
                                 break;
                             case >= 30:
                                 Console.WriteLine(LevelNovice);
-                                Console.WriteLine(TitleNovice);
-                                title = "Arka Nullpointer";
+                                Console.ForegroundColor = ConsoleColor.Blue;
+                                Console.WriteLine(Title + TitleNovice);
+                                Console.ForegroundColor = ConsoleColor.White;
+                                title = TitleNovice;
                                 break;
                             case >= 20:
                                 Console.WriteLine(LevelNoob);
-                                Console.WriteLine(TitleNoob);
-                                title = "Zyn the Bugged";
+                                Console.ForegroundColor = ConsoleColor.Red;
+                                Console.WriteLine(Title + TitleNoob);
+                                Console.ForegroundColor = ConsoleColor.White;
+                                title = TitleNoob;
                                 break;
                             case < 20:
                                 Console.WriteLine(LevelDisaster);
-                                Console.WriteLine(TitleDisaster);
-                                title = "Raoden the Elantrin";
+                                Console.ForegroundColor = ConsoleColor.Gray;
+                                Console.WriteLine(Title + TitleDisaster);
+                                Console.ForegroundColor = ConsoleColor.White;
+                                title = TitleDisaster;
                                 break;
                         }
+                        Console.WriteLine();
                         setWizard = true;
                         break;
                     case 2:
@@ -177,21 +195,30 @@ public class Program
 
                                 do
                                 {
+                                    Console.ForegroundColor = ConsoleColor.Green;
                                     Console.WriteLine(MonsterHp, arrayMonsters[monster], health);
                                     int damage = rnd.Next(MinDamage, MaxDamage);
-                                    Console.WriteLine(arrayDice[damage -1]);
+                                    Console.WriteLine(arrayDice[damage - 1]);
                                     health = health - damage;
                                     Console.WriteLine(DiceResult, (damage));
                                     Console.WriteLine(MonsterDamage, damage);
+                                    Console.WriteLine(PressEnter);
+                                    Console.ReadLine();
                                 } while (health > 0);
-                                
+
                                 level = level + 1;
+                                Console.ForegroundColor = ConsoleColor.Blue;
                                 Console.WriteLine(MonsterDefeat, level);
+                                Console.ReadLine();
+                                Console.ForegroundColor = ConsoleColor.White;
                                 break;
                             case >= 5:
+                                Console.ForegroundColor = ConsoleColor.Yellow;
                                 Console.WriteLine(MaxLevel);
+                                Console.ReadLine();
+                                Console.ForegroundColor = ConsoleColor.White;
                                 break;
-                        }  
+                        }
                         break;
                 }
             }
